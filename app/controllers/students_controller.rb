5df0113f1655259281c75d529282b76f8879abb1
github username <github email address>
2019-04-29 15:21:21 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,16 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+    #  byebug
+  end
+
+  def activate #takes in student param from url.
+    #@student = Student.find(params[:id]) #find specific student
+    set_student
+    @student.update(active: !@student.active) #update student active setting the current active as param
+    #@student.save
+    redirect_to show
   end
 
   private
